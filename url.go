24f41ba72a152dd189975c4195b02d7794ae4e74
Nicolai Dagestad <nicolai@dagestad.fr>
2022-01-08 11:14:45 +0100

    Try to be smart about missing protocols (often a bad idea)

--- url.go
+++ url.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"errors"
-	"path/filepath"
+	"fmt"
 	"net/url"
 	"os"
-	"fmt"
+	"path/filepath"
 )
 
 type URL struct {
@@ -14,37 +14,34 @@ type URL struct {
 }
 
 func Parse(raw_url string) (*URL, error) {
+	var new_url string
 	parsed, err := url.Parse(raw_url)
 	if err != nil {
 		// TODO do something if the parsing fails
 		log("Got an error while parsning: %v\n", err)
 	}
 
-
-	// TODO Add http/https if the URI is a url without scheme
 	if parsed.Scheme == "" {
 		_, err := os.Open(raw_url)
-		if ! errors.Is(err, os.ErrNotExist) {
+		if !errors.Is(err, os.ErrNotExist) { // If it exists, it is a file
 			log("Expanding potential tildes in the path\n")
 			raw_url = expandTilde(raw_url)
 			raw_url, err = filepath.Abs(raw_url)
 			log("Adding the file:// scheme to the url for disambiguation\n")
-			raw_url = fmt.Sprintf("file://%s", raw_url)
-			parsed, err = url.Parse(raw_url)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error in reparsing the url after expanding the tilde %v\n", err)
-				return &URL{
-					raw_url: raw_url,
-					URL: parsed,
-				}, err
-			}
+			new_url = fmt.Sprintf("file://%s", raw_url)
+		} else { // If not we assume it is a URL
+			log("Adding the http:// scheme to the url for disambiguation\n")
+			new_url = fmt.Sprintf("http://%s", raw_url)
+		}
+		parsed, err = url.Parse(new_url)
+		if err != nil {
+			// TODO Trickle down this error message instead of directly printing it?
+			log("Error in reparsing the url after expanding the tilde %v\n", err)
 		}
 	}
 
 	return &URL{
-		raw_url: raw_url,
-		URL: parsed,
+		raw_url: new_url,
+		URL:     parsed,
 	}, err
 }
-
-//func (this *URL) String() string { return "" }
